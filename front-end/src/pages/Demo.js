c7dff5d79a255ffe87499403123a22ea880da9fc
tuannv1104 <tuannv1104@users.noreply.github.com>
2022-12-02 16:39:30 +0700

    mock demo

--- front-end/src/pages/Demo.js
+++ front-end/src/pages/Demo.js
@@ -2,13 +2,121 @@ import * as React from 'react';
 import { Box, Button, Container, FormControl, InputLabel, OutlinedInput, Typography } from '@mui/material';
 import SendIcon from '@mui/icons-material/Send';
 import { Heatmap } from '@ant-design/charts';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import { styled } from '@mui/material/styles';
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+    '& .MuiDialogContent-root': {
+        padding: theme.spacing(2),
+    },
+    '& .MuiDialogActions-root': {
+        padding: theme.spacing(1),
+    },
+}));
+function BootstrapDialogTitle(props: DialogTitleProps) {
+    const { children, onClose, ...other } = props;
+
+    return (
+        <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+            {children}
+            {onClose ? (
+                <IconButton
+                    aria-label="close"
+                    onClick={onClose}
+                    sx={{
+                        position: 'absolute',
+                        right: 8,
+                        top: 8,
+                        color: (theme) => theme.palette.grey[500],
+                    }}
+                >
+                    <CloseIcon />
+                </IconButton>
+            ) : null}
+        </DialogTitle>
+    );
+}
 
 export default function Demo() {
-
+    const [open, setOpen] = React.useState(false);
     const [data, setData] = React.useState([]);
 
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     React.useEffect(() => {
-        asyncFetch();
+        // asyncFetch();
+        setData([
+            {
+                "category": "Thời sự",
+                "category_index": 1,
+                "score": 0.12
+            },
+            {
+                "category": "Văn hóa",
+                "category_index": 2,
+                "score": 0.01
+            },
+            {
+                "category": "Sức khỏe",
+                "category_index": 3,
+                "score": 0.02
+            },
+            {
+                "category": "Giải trí",
+                "category_index": 4,
+                "score": 0.01
+            },
+            {
+                "category": "Tài chính kinh doanh",
+                "category_index": 5,
+                "score": 0.42
+            },
+            {
+                "category": "Thế giới",
+                "category_index": 6,
+                "score": 0.11
+            },
+            {
+                "category": "Giáo dục",
+                "category_index": 7,
+                "score": 0.35
+            },
+            {
+                "category": "Pháp luật",
+                "category_index": 8,
+                "score": 0.21
+            },
+            {
+                "category": "Kinh doanh",
+                "category_index": 9,
+                "score": 0.01
+            },
+            {
+                "category": "Khoa học",
+                "category_index": 10,
+                "score": 0.11
+            },
+            {
+                "category": "Đời sống",
+                "category_index": 11,
+                "score": 0.25
+            },
+            {
+                "category": "Du lịch",
+                "category_index": 12,
+                "score": 0.99
+            }
+        ]);
     }, []);
 
     const asyncFetch = () => {
@@ -24,12 +132,12 @@ export default function Demo() {
         height: 500,
         autoFit: false,
         data,
-        xField: 'Month of Year',
-        yField: 'District',
-        colorField: 'AQHI',
+        xField: 'category',
+        yField: 'score',
+        colorField: 'score',
         color: ['#174c83', '#7eb6d4', '#efefeb', '#efa759', '#9b4d16'],
         meta: {
-            'Month of Year': {
+            'category': {
                 type: 'cat',
             },
         },
@@ -38,10 +146,11 @@ export default function Demo() {
     return (
         <Container sx={{
             marginTop: "75px"
+
         }}>
-            {/* <Typography variant="h4" component="h2" align='center' >
+            <Typography variant="h4" component="h3" align='left' sx={{ padding: "8px" }} color="primary" >
                 Demo
-            </Typography> */}
+            </Typography>
             <FormControl fullWidth sx={{ m: 1 }}>
                 <InputLabel htmlFor="outlined-adornment-title">Tiêu đề</InputLabel>
                 <OutlinedInput
@@ -69,7 +178,7 @@ export default function Demo() {
             }}>
                 <Button variant="contained" size='large' style={{ background: 'linear-gradient(to right bottom, #0061ff, #60efff)' }} endIcon={<SendIcon />} sx={{
                     margin: "8px",
-                }}>
+                }} onClick={handleClickOpen}>
                     Send
                 </Button>
 
@@ -79,9 +188,27 @@ export default function Demo() {
                 justifyContent: 'center'
 
             }}>
-                <Heatmap {...config} />
+
 
             </Box>
+            <BootstrapDialog
+                onClose={handleClose}
+                aria-labelledby="customized-dialog-title"
+                open={open}
+                maxWidth='lg'
+            >
+                <BootstrapDialogTitle id="customized-dialog-title" onClose={handleClose}>
+                    Result
+                </BootstrapDialogTitle>
+                <DialogContent dividers>
+                    <Heatmap {...config} />
+                </DialogContent>
+                <DialogActions>
+                    <Button autoFocus onClick={handleClose}>
+                        Save changes
+                    </Button>
+                </DialogActions>
+            </BootstrapDialog>
         </Container>
     );
 }
