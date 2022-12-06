5507e35e7702c5223c4f11a6a36bab944a28dcfc
tuannv1104 <tuannv1104@users.noreply.github.com>
2022-12-06 09:50:59 +0700

    update demo

--- front-end/src/pages/Demo.js
+++ front-end/src/pages/Demo.js
@@ -65,7 +65,7 @@ export default function Demo() {
 
             const response = await axios.post(`http://localhost:8101/api/predict`, articles);
             console.log(response.data.data[0].result);
-            setData(response.data.data[0].result);
+            setData(response.data.data[0].result.sort(function (a, b) { return b.score - a.score }));
 
         } catch (error) {
             console.error(error);
